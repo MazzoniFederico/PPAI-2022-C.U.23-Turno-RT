0345f89a6086dddd96ccccdea2503018500eb783
Federico Mazzoni <80524@sistemas.frc.utn.edu.ar>
2022-10-05 21:51:55 -0300

    Creacion interfaz y estrategias concretas

---
+++ PPAI 2022 C.U.23 Turno RT/Negocios/ConAntelacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI_2022_C.U._23_Turno_RT.Negocios
+{
+    class ConAntelacion : IEstrategiaAntelacion
+    {
+        public List<string> buscarTurnos(DateTime fechaActual, RecursoTecnologico recursoTecnologicoSeleccionado)
+        {
+            return recursoTecnologicoSeleccionado.buscarTurnoPosteriorFechaActual(fechaActual);
+        }
+    }
+}
